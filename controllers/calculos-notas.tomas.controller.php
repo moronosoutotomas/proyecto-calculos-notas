98baf79f19a7e0ee9117ad77e4ae64261a8437b3
Tomás <moronosoutotomas.sangrina@gmail.com>
2024-10-13 10:09:47 +0200

    Función checkForm terminada, pasamos al procesado

--- controllers/calculos-notas.tomas.controller.php
+++ controllers/calculos-notas.tomas.controller.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpComposerExtensionStubsInspection */
 declare(strict_types=1);
 
 $data = [];
@@ -17,7 +17,7 @@ if (isset($_POST['enviar'])) {
     // Mostramos errores
     $data['errores'] = $errores;
   } else {
-
+//var_dump($errores);
     // Procesamos
     $data['resultado'] = procesarTexto($_POST['input']);
   }
@@ -35,28 +35,39 @@ function checkForm(string $datos): array
     $errores['texto'] = 'Error: este campo es obligatorio';
   } else {
     $descodificado = json_decode(trim($datos), true);
-    if (is_null($descodificado)) {
+    if (is_null($descodificado)) { // json_decode() devuelve nulos si no reconoce el formato
       $errores['texto'][] = 'Error: introduzca un bloque en formato JSON válido';
     } else {
+
+      // Iteración asignaturas
       foreach ((array)$descodificado as $asignatura => $alumnado) {
         if ((!is_string($asignatura)) || (mb_strlen(trim($asignatura)) < 1)) {
-          $errores['texto'][] = "Error: el formato de texto de la asignatura '$asignatura' no es de un tipo válido";
+          $errores['texto'][] = "Error: el formato de texto de la asignatura '$asignatura' no es válido";
         }
         if (!is_array($alumnado)) {
           $errores['texto'][] = "Error: cada asignatura debe contener un conjunto de alumnos ('$alumnado' no es un array)";
         } else {
-          foreach ($alumnado as $alumno => $nota) {
+
+          // Iteración alumnos
+          foreach ($alumnado as $alumno => $boletinNotas) {
             if (!is_string($alumno) || (mb_strlen(trim($alumno)) < 1)) {
-              $errores['texto'][] = "Error: el formato de texto del nombre del alumno '$alumno' no es de un tipo válido";
+              $errores['texto'][] = "Error: el formato de texto del nombre del alumno '$alumno' no es válido";
             }
-            if (!is_numeric($nota)) {
-              $errores['texto'][] = "Error: el formato numérico de la nota '$nota' del alumno '$alumno' no es de un tipo válido";
-            } else if ($nota > 10 || $nota < 0) {
-              $errores['texto'][] = "Error: la nota '$nota' del alumno '$alumno' no está comprendida entre 0-10";
-            }
+
+            // Iteración boletines de notas
+            foreach ($boletinNotas as $nota) {
+              if (!is_numeric($nota)) {
+                $errores['texto'][] = "Error: el formato numérico de la nota '$nota' de '$alumno' no es válido";
+              } else if ($nota > 10 || $nota < 0) {
+                $errores['texto'][] = "Error: la nota '$nota' del alumno '$alumno' no está comprendida entre 0-10";
+              }
+            }// foreach $nota
+
           } //foreach $alumnado
+
         }
       } //foreach $asignaturas
+
     }
   }
   return $errores;
@@ -64,7 +75,7 @@ function checkForm(string $datos): array
 
 /**
  * Función que procesa los datos y realiza los cálculos
- * @param string $input
+ * @param string $datos
  * @return array
  */
 function procesarTexto(string $datos): array
